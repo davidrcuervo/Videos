24b659411001d569bf56225df40db8469dd2695a
I849921 <I849921@YMQN00585838A.amer.global.corp.sap>
2016-08-16 14:49:08 -0400

    charArrayWriterResponse

--- Videos/src/com/laetienda/utilities/CharArrayWriterResponse.java
+++ Videos/src/com/laetienda/utilities/CharArrayWriterResponse.java
@@ -3,9 +3,15 @@ package com.laetienda.utilities;
 import java.io.CharArrayWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.http.HttpServletResponseWrapper;
+import javax.servlet.http.HttpServletResponse;
 
-public class CharArrayWriterResponse {
+public class CharArrayWriterResponse extends HttpServletResponseWrapper{
 	
 	private final CharArrayWriter charArray = new CharArrayWriter();
- 
+	
+	public CharArrayWriterResponse(HttpServletResponse response){
+		super(response);
+	}
+	
 }
